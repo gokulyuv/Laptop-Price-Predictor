f69f6a2e527cb765a7b68288d02f9fd044622fc6
gokul krishnan <65410203+gokulyuv@users.noreply.github.com>
2020-08-03 10:41:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,22 +22,11 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    RAM = int(request.form['RAM'])
-    HDD = int(request.form['HDD'])
-    SSD1 = int(request.form['SSD1'])
-    SSD2 = int(request.form['SSD2'])
-    OS = int(request.form['OS'])
-    CHIPSET = int(request.form['CHIPSET'])
-    SCREEN_SIZE = int(request.form['SCREEN_SIZE'])
-    RESOLUTION = int(request.form['RESOLUTION'])
-    GRAPHICS_CARD = int(request.form['GRAPHICS_CARD'])
-    GPU_MEMORY = int(request.form['GPU_MEMORY'])
-    #prediction = model.predict([[RAM,HDD,SSD1,SSD2,OS,CHIPSET,SCREEN_SIZE,RESOLUTION,GRAPHICS_CARD,GPU_MEMORY]])
-    List=[RAM,HDD,SSD1,SSD2,OS,CHIPSET,SCREEN_SIZE,RESOLUTION,GRAPHICS_CARD,GPU_MEMORY]
-    array=np.array(List)
-    array1=np.reshape(array,(1,-1))
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
     #output = round(prediction[0], 2)
-    prediction=model.predict(array1)
+    
  
     return render_template('index.html', prediction_text='LAPTOP VALUE should be : Rs. {}'.format(prediction))
     
