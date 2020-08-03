a3dad474546e3a70a7f0969ac363813d960dc4e8
gokul krishnan <65410203+gokulyuv@users.noreply.github.com>
2020-08-03 11:20:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def predict():
     #output = round(prediction[0], 2)
     
  
-    return render_template('index.html', prediction_text='LAPTOP VALUE should be : Rs. {}'.format(final_features.shape))
+    return render_template('index.html', prediction_text='LAPTOP VALUE should be : Rs. {}'.format(prediction))
     
     
 
